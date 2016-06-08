05406d3798173235355b484e9c942cb69cfb0ac3
Nassau Student <nassaustudent@users.noreply.github.com>
2016-06-08 14:53:33 -0300

    Classe Controle dos clientes
    Classe Controle dos clientes

---
+++ src/control/ClienteControl.java
@@ -0,0 +1,33 @@
+package control;
+
+import java.util.ArrayList;
+import model.Cliente;
+
+
+public class ClienteControl {
+
+//classe do controle que cadastra clientes
+    public static boolean cadastrarCliente(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+//classe do controle que deleta cliente
+    public static boolean deletarCliente(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+//classe do controle que edita cliente  
+    public static boolean editarCliente(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+//classe do controle que lista cliente
+    public static ArrayList<Cliente> listarCliente() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+
+    
+    
+}
